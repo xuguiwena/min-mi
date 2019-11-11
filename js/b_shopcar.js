419b55d4c4b642ba140ae534ee1f4fa39ed49f7f
yuwen <1398945292@qq.com>
2019-11-11 17:53:31 +0800

    b

--- js/b_shopcar.js
+++ js/b_shopcar.js
@@ -4,13 +4,13 @@ var b_nane = document.querySelector('.b_nane')
 var b_p = document.querySelector('.b_p')
 var b_btn2 = document.querySelector('.b_btn2')
 var b_btn1 = document.querySelector('.b_btn1')
-var b_shang=document.querySelector('.b_shang')
-var b_wenzi=document.querySelector('.b_wenzi')
-var b_gwc=document.querySelector('.b_gwc')
-var b_splb=document.querySelector('.b_splb')
-var b_shop=document.querySelector('.b_shop')
-var b_b1=document.querySelector('.b_b1')
-var b_b2=document.querySelector('.b_b2')
+var b_shang = document.querySelector('.b_shang')
+var b_wenzi = document.querySelector('.b_wenzi')
+var b_gwc = document.querySelector('.b_gwc')
+var b_splb = document.querySelector('.b_splb')
+var b_shop = document.querySelector('.b_shop')
+var b_b1 = document.querySelector('.b_b1')
+var b_b2 = document.querySelector('.b_b2')
 
 
 
@@ -20,38 +20,38 @@ jia()
 
 
 ajax({
-    url:'./data/b_gwc.json',
-    type:'get',
-    data:'',
-    success:function(data){
-        var json=JSON.parse(data)
-        var str=''
-        for(var i=0,len=json.length;i<len;i++){
-            str+='<div class="b_s1" code='+json[i].code+'><div><img src='+json[i].imgurl+' alt=""></div><h3>'+json[i].type+'</h3><strong>'+json[i].price+'</strong><span>加入购物车</span></div>'
+    url: './data/b_gwc.json',
+    type: 'get',
+    data: '',
+    success: function (data) {
+        var json = JSON.parse(data)
+        var str = ''
+        for (var i = 0, len = json.length; i < len; i++) {
+            str += '<div class="b_s1" code=' + json[i].code + '><div><img src=' + json[i].imgurl + ' alt=""></div><h3>' + json[i].type + '</h3><strong>' + json[i].price + '</strong><span>加入购物车</span></div>'
         }
-        b_shang.innerHTML=str
+        b_shang.innerHTML = str
         //添加购物车
         //localStorage -> shop : '{"code":['abc1','abc4','abc6']}'
-        $('.b_s1>span').on('click',function(){
-            var code=$(this).parent().attr('code')
-            if(localStorage.getItem("shop")){
-                arrCode=JSON.parse(localStorage.getItem('shop')).code
-                num=JSON.parse(localStorage.getItem('shop')).num
-            }else{
-                var arrCode=[]
-                var num=[]
+        $('.b_s1>span').on('click', function () {
+            var code = $(this).parent().attr('code')
+            if (localStorage.getItem("shop")) {
+                arrCode = JSON.parse(localStorage.getItem('shop')).code
+                num = JSON.parse(localStorage.getItem('shop')).num
+            } else {
+                var arrCode = []
+                var num = []
             }
-            if(arrCode.indexOf(code)<0){
+            if (arrCode.indexOf(code) < 0) {
                 arrCode.push(code)
                 num.push(1)
                 alert('商品添加成功')
-            }else{
-                var ind=num[arrCode.indexOf(code)]
-                num.splice(arrCode.indexOf(code),1,ind-0+1)
+            } else {
+                var ind = num[arrCode.indexOf(code)]
+                num.splice(arrCode.indexOf(code), 1, ind - 0 + 1)
                 alert('商品添加成功')
             }
-            var Json=JSON.stringify({'code':arrCode,'num':num})
-            localStorage.setItem('shop',Json)
+            var Json = JSON.stringify({ 'code': arrCode, 'num': num })
+            localStorage.setItem('shop', Json)
             location.reload();
         })
     }
@@ -62,22 +62,22 @@ ajax({
 
 han()
 
-function han(){
-    if(localStorage.getItem("shop")){
-        var arrcd=JSON.parse(localStorage.getItem("shop")).code
-        var arrnum=JSON.parse(localStorage.getItem("shop")).num
+function han() {
+    if (localStorage.getItem("shop")) {
+        var arrcd = JSON.parse(localStorage.getItem("shop")).code
+        var arrnum = JSON.parse(localStorage.getItem("shop")).num
         // console.log(arrcd)
-        var str=''
+        var str = ''
         ajax({
-            url:'./data/b_gwc.json',
-            type:'get',
-            data:'',
-            success:function(data){
-                var json=JSON.parse(data)
-                for(var i=0,len=json.length;i<len;i++){
-                    for(var j=0;j<arrcd.length;j++){
-                        if(json[i].code==arrcd[j]){
-                            str+=`<div class="b_gwccot clearfix" code=${json[i].code}>
+            url: './data/b_gwc.json',
+            type: 'get',
+            data: '',
+            success: function (data) {
+                var json = JSON.parse(data)
+                for (var i = 0, len = json.length; i < len; i++) {
+                    for (var j = 0; j < arrcd.length; j++) {
+                        if (json[i].code == arrcd[j]) {
+                            str += `<div class="b_gwccot clearfix" code=${json[i].code}>
                             <div>
                                 <input type="checkbox" class="checkAll">
                                 <span></span>
@@ -92,95 +92,95 @@ function han(){
                                     <a href="javascript:;" class="btn_right">+</a>
                                 </div>
                             </div>
-                            <div>${parseInt(json[i].price)*arrnum[j]}元</div>
+                            <div>${parseInt(json[i].price) * arrnum[j]}元</div>
                             <div><span class="b_remove">X</span></div>
                         </div>`
                         }
                     }
                 }
-                b_splb.innerHTML=str
+                b_splb.innerHTML = str
                 // 商品数量
-                $('.b_gwccot').on('click','.btn_left',function(){
-                    $(this).next().val($(this).next().val()-1)
-                    if($(this).next().val()<1){
+                $('.b_gwccot').on('click', '.btn_left', function () {
+                    $(this).next().val($(this).next().val() - 1)
+                    if ($(this).next().val() < 1) {
                         $(this).next().val(1)
                     }
-                    arrcd=JSON.parse(localStorage.getItem("shop")).code
-                    arrnum=JSON.parse(localStorage.getItem("shop")).num
-                    var dex=arrcd.indexOf($(this).parent().parent().parent().attr('code'))
-                    arrnum[dex]=$(this).next().val()
-                    var shopobj=JSON.stringify({"code":arrcd,"num":arrnum})
-                    localStorage.setItem("shop",shopobj)
+                    arrcd = JSON.parse(localStorage.getItem("shop")).code
+                    arrnum = JSON.parse(localStorage.getItem("shop")).num
+                    var dex = arrcd.indexOf($(this).parent().parent().parent().attr('code'))
+                    arrnum[dex] = $(this).next().val()
+                    var shopobj = JSON.stringify({ "code": arrcd, "num": arrnum })
+                    localStorage.setItem("shop", shopobj)
                     han()
                 })
-    
-                $('.b_gwccot').on('click','.btn_right',function(){
-                    $(this).prev().val($(this).prev().val()-0+1)
-                    arrcd=JSON.parse(localStorage.getItem("shop")).code
-                    arrnum=JSON.parse(localStorage.getItem("shop")).num
-                    var dex=arrcd.indexOf($(this).parent().parent().parent().attr('code'))
-                    arrnum[dex]=$(this).prev().val()
-                    var shopobj=JSON.stringify({"code":arrcd,"num":arrnum})
-                    localStorage.setItem("shop",shopobj)
+
+                $('.b_gwccot').on('click', '.btn_right', function () {
+                    $(this).prev().val($(this).prev().val() - 0 + 1)
+                    arrcd = JSON.parse(localStorage.getItem("shop")).code
+                    arrnum = JSON.parse(localStorage.getItem("shop")).num
+                    var dex = arrcd.indexOf($(this).parent().parent().parent().attr('code'))
+                    arrnum[dex] = $(this).prev().val()
+                    var shopobj = JSON.stringify({ "code": arrcd, "num": arrnum })
+                    localStorage.setItem("shop", shopobj)
                     han()
                 })
 
 
                 // 全选
-                $('#checkid').click(function(){
-                   $('.b_splb input').prop('checked',$(this).prop('checked'))
-                   var a=0
-                   var sum=0
-                    for(var i=0;i<$('.checkAll').length;i++){
-                        if($('.checkAll').eq(i).prop('checked')){
-                            a+=1
-                            sum+=parseInt($('.checkAll').eq(i).parent().next().next().next().next().next().html())
+                $('#checkid').click(function () {
+                    $('.b_splb input').prop('checked', $(this).prop('checked'))
+                    var a = 0
+                    var sum = 0
+                    for (var i = 0; i < $('.checkAll').length; i++) {
+                        if ($('.checkAll').eq(i).prop('checked')) {
+                            a += 1
+                            sum += parseInt($('.checkAll').eq(i).parent().next().next().next().next().next().html())
                         }
                     }
                     $('.b_b2').html(a)
                     $('.b_em').html(sum)
                 })
                 // 单选
-                $('.b_splb').on('click','.checkAll',function(){
-                    var bool=true;
-                    $('.checkAll').each(function(index,val){
-                        if(!$(val).prop('checked')){
-                            bool=false
-                            $('#checkid').prop('checked',false)
+                $('.b_splb').on('click', '.checkAll', function () {
+                    var bool = true;
+                    $('.checkAll').each(function (index, val) {
+                        if (!$(val).prop('checked')) {
+                            bool = false
+                            $('#checkid').prop('checked', false)
                             return;
                         }
                     });
-                    $('#checkid').prop('checked',bool)
-                    var a=0
-                    var sum=0
-                    for(var i=0;i<$('.checkAll').length;i++){
-                        if($('.checkAll').eq(i).prop('checked')){
-                            a+=1
-                            sum+=parseInt($('.checkAll').eq(i).parent().next().next().next().next().next().html())
+                    $('#checkid').prop('checked', bool)
+                    var a = 0
+                    var sum = 0
+                    for (var i = 0; i < $('.checkAll').length; i++) {
+                        if ($('.checkAll').eq(i).prop('checked')) {
+                            a += 1
+                            sum += parseInt($('.checkAll').eq(i).parent().next().next().next().next().next().html())
                         }
                     }
                     $('.b_b2').html(a)
                     $('.b_em').html(sum)
                 })
-                
+
             }
         })
-        
+
     }
 }
 
 
 // 删除
-$('.b_splb').on('click','.b_remove',function(){
-    arrcd=JSON.parse(localStorage.getItem("shop")).code
-    arrnum=JSON.parse(localStorage.getItem("shop")).num
-    var index=arrcd.indexOf($(this).parent().parent().attr('code'))
-    console.log(arrcd,arrnum)
-    arrcd.splice(index,1)
-    arrnum.splice(index,1)
-    console.log(arrcd,arrnum)
-    var shopobj=JSON.stringify({"code":arrcd,"num":arrnum})
-    localStorage.setItem("shop",shopobj)
+$('.b_splb').on('click', '.b_remove', function () {
+    arrcd = JSON.parse(localStorage.getItem("shop")).code
+    arrnum = JSON.parse(localStorage.getItem("shop")).num
+    var index = arrcd.indexOf($(this).parent().parent().attr('code'))
+    console.log(arrcd, arrnum)
+    arrcd.splice(index, 1)
+    arrnum.splice(index, 1)
+    console.log(arrcd, arrnum)
+    var shopobj = JSON.stringify({ "code": arrcd, "num": arrnum })
+    localStorage.setItem("shop", shopobj)
     $(this).parent().parent().remove()
     jia()
 })
@@ -200,11 +200,11 @@ lis[4].onclick = function () {
 
 
 
-b_btn1.onclick=function(){
-    location.href='b_login.html'
+b_btn1.onclick = function () {
+    location.href = 'b_login.html'
 }
-b_btn2.onclick=function(){
-    location.href='index.html'
+b_btn2.onclick = function () {
+    location.href = 'index.html'
 }
 
 
@@ -214,7 +214,7 @@ b_btn2.onclick=function(){
 
 
 
-function ajax(option){
+function ajax(option) {
     // 1.创建XMLHttpRequest对象(数据交互对象)
     if (window.XMLHttpRequest) {
         var xhr = new XMLHttpRequest();
@@ -224,14 +224,14 @@ function ajax(option){
     // data -> 'a=123&b=456'
     if (option.type == 'get' || option.type == 'GET') {
         // 2.打开与服务器的链接
-        xhr.open(option.type,option.url + '?'+ option.data + '&_='+new Date().getTime(),true);//解决缓存
+        xhr.open(option.type, option.url + '?' + option.data + '&_=' + new Date().getTime(), true);//解决缓存
         // 3.发送请求
         xhr.send(null);//get请求
-    } else if (option.type == 'post' || option.type == 'POST'){
+    } else if (option.type == 'post' || option.type == 'POST') {
         // 2.打开与服务器的链接
-        xhr.open(option.type,option.url,true);//解决缓存
+        xhr.open(option.type, option.url, true);//解决缓存
         // 模拟表单form的post方式提交数据，在send之前设置
-        xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
         // 3.发送请求
         xhr.send(option.data);//post请求
     } else {
@@ -239,12 +239,12 @@ function ajax(option){
     };
 
     // 4.等待服务的响应
-    xhr.onreadystatechange = function (){
+    xhr.onreadystatechange = function () {
         // console.log(xhr.readyState);//2 3 4
         if (xhr.readyState == 4) {//请求完成
             if (xhr.status == 200) {//请求成功
                 option.success(xhr.responseText);
-            }else {//请求失败
+            } else {//请求失败
                 option.failed(xhr.status);
             }
         }
@@ -268,28 +268,34 @@ function jia() {
         b_btn2.style.display = 'inline-block'
         b_btn1.style.display = 'inline-block'
     }
+    if (JSON.parse(localStorage.getItem("shop"))) {
+        if (JSON.parse(localStorage.getItem("shop")).code.length > 0) {
+            b_gwc.style.background = 'none'
+            b_shop.style.display = 'block'
+            b_wenzi.style.display = 'none'
 
-    if(JSON.parse(localStorage.getItem("shop")).code.length>0){
-        b_gwc.style.background='none'
-        b_shop.style.display='block'
-        b_wenzi.style.display='none'
-        
+        } else {
+            b_gwc.style.background = 'url("img/b_gwcimg/b1.png") no-repeat 120px 0'
+            b_wenzi.style.display = 'block'
+            b_shop.style.display = 'none'
+        }
+
+        var arrcd = JSON.parse(localStorage.getItem("shop")).code
+        b_b1.innerHTML = arrcd.length
     }else{
-        b_gwc.style.background='url("img/b_gwcimg/b1.png") no-repeat 120px 0'
-        b_wenzi.style.display='block'
-        b_shop.style.display='none'
+        b_gwc.style.background = 'url("img/b_gwcimg/b1.png") no-repeat 120px 0'
+        b_wenzi.style.display = 'block'
+        b_shop.style.display = 'none'
     }
 
-    var arrcd=JSON.parse(localStorage.getItem("shop")).code
-    b_b1.innerHTML=arrcd.length
 
 
-    var a=0
-    var sum=0
-    for(var i=0;i<$('.checkAll').length;i++){
-        if($('.checkAll').eq(i).prop('checked')){
-            a+=1
-            sum+=parseInt($('.checkAll').eq(i).parent().next().next().next().next().next().html())
+    var a = 0
+    var sum = 0
+    for (var i = 0; i < $('.checkAll').length; i++) {
+        if ($('.checkAll').eq(i).prop('checked')) {
+            a += 1
+            sum += parseInt($('.checkAll').eq(i).parent().next().next().next().next().next().html())
         }
     }
     $('.b_b2').html(a)
